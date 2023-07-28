1469f56df4908f0d11b55b5a62a522c8838fc3e8
Fabricio Trindade <fabrtrindade@gmail.com>
2023-07-28 08:51:17 -0300

    Example 2 to implement a button click handle: by binding

--- app/src/main/java/com/fabscorp/travelexpenses/MainActivity.kt
+++ app/src/main/java/com/fabscorp/travelexpenses/MainActivity.kt
@@ -5,15 +5,21 @@ import android.os.Bundle
 import android.view.View
 import com.fabscorp.travelexpenses.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+    override fun onClick(view: View) {
+        TODO("Not yet implemented")
     }
 
     fun btnCalculate(view: View) {
-        
+
     }
+
 }
